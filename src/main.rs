1c9b6120af5c9e6a0ee2291238d8fbf331546a8c
Murilo Damario <murilo.gcdamario@gmail.com>
2026-01-07 23:41:32 -0300

    :sparkles: feat: setup project and implement basic image metadata extraction

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use image::{DynamicImage, GenericImageView, Rgba};
+
 fn main() {
-    println!("Hello, world!");
+    let img: DynamicImage =
+        image::open("src/images/Albert_Einstein.png").expect("Failed to open image");
+
+    let (width, height) = img.dimensions();
+    println!("Width: {}px, Height: {}px", width, height);
+
+    let pixel: Rgba<u8> = img.get_pixel(0, 0);
+
+    let r: u8 = pixel.0[0];
+    let g: u8 = pixel.0[1];
+    let b: u8 = pixel.0[2];
+    let a: u8 = pixel.0[3];
+
+    println!("Pixel at (0, 0): R={}, G={}, B={}, A={}", r, g, b, a);
+
+    let total_pixels: u64 = (width as u64) * (height as u64);
+
+    let total_bits_capacity: u64 = total_pixels * 4;
+
+    let total_bytes_capacity: u64 = total_bits_capacity / 8;
+
+    println!("\n--- Statistics ---");
+    println!("Total Pixels  : {}", total_pixels);
+    println!("Bit Capacity  : {} bits", total_bits_capacity);
+    println!(
+        "Byte Capacity : {} bytes (~{} KB)",
+        total_bytes_capacity,
+        total_bytes_capacity / 1024
+    );
+
+    println!("Max Message   : {} characters", total_bytes_capacity);
 }
